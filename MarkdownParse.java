ccc40cabe3dcb0edf868570196ae4491a0388432
Prodlax <50209414+Prodlax@users.noreply.github.com>
2022-01-20 16:43:55 -0800

    fixed scenarios when we have [] and () that does not refer to a link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,10 +17,9 @@ public class MarkdownParse {
             {
                 break;
             }
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            int nextCloseBracket = markdown.indexOf("](", nextOpenBracket);
+            int closeParen = markdown.indexOf(")", nextCloseBracket);
+            toReturn.add(markdown.substring(nextCloseBracket + 2, closeParen));
             currentIndex = closeParen + 1;
             System.out.println(currentIndex);
             
